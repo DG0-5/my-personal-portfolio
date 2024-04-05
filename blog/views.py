5b9ccd884e3094cb8c5c6379e909c54dc5413975
dg0-5 <dg0.5@outlook.com>
2024-04-04 21:12:53 -0500

    Make Changes

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
 
-# def all_blogs(request):
-#     return render(request,'blog/all_blogs.html')
-
 
 def all_blogs(request):
     blogs = Blog.objects.all()
